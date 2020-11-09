1d1a57a1bd47432100fe2534f9d9b6d1431d6e52
Bakare Faruq <bakarefaruq.a@gmail.com>
2020-11-09 14:52:38 -0800

    basic form setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("")
+  const [messages, setMessages] = useState([])
+  
+  console.log(messages)
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+    setMessages([...messages, input])
+    setInput("")
+  }
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      <form> 
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input type="text" value={input} onChange={ e => setInput(e.target.value)} />
+        </FormControl>
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      </form>
+      
+      {
+        messages.map(message => {
+          return <p>{message}</p>
+        })
+      }
     </div>
   );
 }
